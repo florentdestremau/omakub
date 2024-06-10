3f52478cbb9688bf37d6d3df1b99050ddefca6c8
Florent Destremau <florentdestremau@users.noreply.github.com>
2024-06-10 16:04:42 +0200

    Update php.sh

--- install/php.sh
+++ install/php.sh
@@ -1,5 +1,6 @@
 sudo add-apt-repository -y ppa:ondrej/php
 
 sudo apt -y install php8.3 php8.3-{bcmath,bz2,curl,apcu,intl,gd,mbstring,opcache,pgsql,redis,xml,zip,fpm} \
-  php-gd php-gmp php-curl zip unzip php-igbinary composer
+  php-gd php-gmp php-curl php-igbinary composer
+
 curl -sS https://get.symfony.com/cli/installer | bash

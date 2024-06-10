cca48eb3ac93be73ef89b1656018294d10da6c24
Florent Destremau <florentdestremau@users.noreply.github.com>
2024-06-10 16:11:53 +0200

    Update php.sh

--- install/php.sh
+++ install/php.sh
@@ -3,4 +3,6 @@ sudo add-apt-repository -y ppa:ondrej/php
 sudo apt -y install php8.3 php8.3-{bcmath,bz2,curl,apcu,intl,gd,mbstring,opcache,pgsql,redis,xml,zip,fpm} \
   php-gd php-gmp php-curl php-igbinary composer
 
-curl -sS https://get.symfony.com/cli/installer | bash
+# install the Symfony command line
+curl -1sLf 'https://dl.cloudsmith.io/public/symfony/stable/setup.deb.sh' | sudo -E bash
+sudo apt -y install symfony-cli

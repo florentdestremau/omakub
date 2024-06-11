29eefa033289e5e235fa54160cc13f9964b03464
Florent Destremau <florent@windoo.fr>
2024-06-11 14:04:59 +0200

    Phpstorm and shell aliases

--- install/app-phpstorm.sh
+++ install/app-phpstorm.sh
@@ -1,1 +1,11 @@
 sudo snap install phpstorm --classic
+
+## native phpstorm:// links
+cd ~/Downloads
+git clone git@github.com:sanduhrs/phpstorm-url-handler.git
+cd phpstorm-url-handler/
+
+sudo cp phpstorm-url-handler /usr/bin/phpstorm-url-handler
+sudo chmod +x /usr/bin/phpstorm-url-handler
+
+cd ~/

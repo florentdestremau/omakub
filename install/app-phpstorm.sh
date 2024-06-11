2b92ff808d0fbd24ab9b94b30d4d5616ee3f5f69
Florent Destremau <florent@windoo.fr>
2024-06-11 14:33:08 +0200

    Updated phpstorm script

--- install/app-phpstorm.sh
+++ install/app-phpstorm.sh
@@ -1,11 +1,12 @@
 sudo snap install phpstorm --classic
 
 ## native phpstorm:// links
-cd ~/Downloads
-git clone git@github.com:sanduhrs/phpstorm-url-handler.git
-cd phpstorm-url-handler/
+git clone git@github.com:sanduhrs/phpstorm-url-handler.git ~/Downloads/phpstorm-url-handler
+cd ~/Downloads/phpstorm-url-handler/
 
 sudo cp phpstorm-url-handler /usr/bin/phpstorm-url-handler
 sudo chmod +x /usr/bin/phpstorm-url-handler
 
-cd ~/
+rm ~/Downloads/phpstorm-url-handler -rf
+
+cd -
